18b588cebc54329677e51c06fbbc554b78d0e5ee
6299481145 <64630034+6299481145@users.noreply.github.com>
2020-07-09 18:18:35 +0530

    WeekName_20200709

--- VBNET/WeekName/WeekName/FrmWeekNameApp.Designer.vb
+++ VBNET/WeekName/WeekName/FrmWeekNameApp.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class FrmPalindromeApp
+Partial Class FrmWeekNameApp
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,30 +22,17 @@ Partial Class FrmPalindromeApp
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(100, 67)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(51, 17)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
-        '
-        'FrmPalindromeApp
+        'FrmWeekNameApp
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(532, 450)
-        Me.Controls.Add(Me.Label1)
-        Me.Name = "FrmPalindromeApp"
-        Me.Text = "PlindromeApp"
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Name = "FrmWeekNameApp"
+        Me.Text = "WeekNameApp"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
 End Class
