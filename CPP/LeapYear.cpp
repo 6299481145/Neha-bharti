ba0da6df83315eabe01fcbac8864f520f0fe6a98
6299481145 <64630034+6299481145@users.noreply.github.com>
2020-07-13 16:25:21 +0530

    VB_Assignment_20200713

---
+++ CPP/LeapYear.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main()   
+{
+	int yr; 
+	cout<<"Please enter a year:";
+	cin>>yr;
+	
+	if(yr%400==0)
+	{
+		cout<<"leap Year";
+	}
+	else
+	{
+	    cout<<"Common Year"	;
+	}
+	return 0;
+}
---
+++ CPP/compound_interest.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+ float p,r,t,ci;	
+ cout<<"Please enter Principal:";
+ cin>>p;
+ 
+ cout<<"Please enter rate:"; 
+ cin>>r;
+
+ cout<<"Please enter time:";
+ cin>>t;
+
+ ci=pow(p*((1+r)/100 ),t);
+
+ cout<<"The compound interest = " <<ci;
+
+ return 0;
+}
