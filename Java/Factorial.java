dd1824546b3e7889b5e72df15d75d26d3d72cad9
6299481145 <64630034+6299481145@users.noreply.github.com>
2020-08-24 12:30:16 +0530

    Commit_20200824

---
+++ Java/Factorial.java
@@ -0,0 +1,30 @@
+//program to find factorial of a given number using function
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main (String[] args){
+	    int n;
+	
+	    Scanner sc= new Scanner(System.in);
+	
+	    System.out.print("Please enter a number:");
+	    n=sc.nextInt();
+	
+	    int f=factorial(n);
+	
+	    System.out.print("The factorial of  " + n + " is " + f + ".");
+	
+	}
+	
+    static int factorial (int n){
+		int fact = 1;
+		
+		for(int i=n; i>1; i--)
+		{
+			fact = fact * i;
+		}
+            return fact;
+	}
+}	
