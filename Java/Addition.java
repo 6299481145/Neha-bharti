ab558a41591b1128e5eaa49400311f95c885cbc2
6299481145 <64630034+6299481145@users.noreply.github.com>
2020-08-24 16:59:47 +0530

    Commit_20200824

--- Java/Addition.java
+++ Java/Addition.java
@@ -3,9 +3,9 @@
 import java.lang.*;
 import java.util.Scanner;
 
-public class AddTwoIntegers {
+public class Addition {
 	
-	public Static void main(String[] args) {
+	public static void main(String[] args) {
 		int a,b,c;
 		
 		Scanner sc = new Scanner(System.in);
@@ -18,7 +18,7 @@ public class AddTwoIntegers {
 		
 		c = a + b;
 		
-		System.out.println("The sum is:"  + c );
+		System.out.println("The sum is "  + c + "." );
 	}
 	
 }
