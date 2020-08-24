821fab0b95956c4b13998e456d23709e29a6e738
6299481145 <64630034+6299481145@users.noreply.github.com>
2020-08-24 13:56:54 +0530

    Commit_20200824

---
+++ Java/Addition.java
@@ -0,0 +1,24 @@
+//Program to add two integers
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class AddTwoIntegers {
+	
+	public Static void main(String[] args) {
+		int a,b,c;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Please enter first number:");
+		a = sc.nextInt();
+		
+		System.out.print("Please enter second number:");
+		b = sc.nextInt();
+		
+		c = a + b;
+		
+		System.out.println("The sum is:"  + c );
+	}
+	
+}
