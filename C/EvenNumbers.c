47c8a5a4430746b0f760f69f28c4e84ba3bf216e
6299481145 <64630034+6299481145@users.noreply.github.com>
2020-08-31 17:18:59 +0530

    Commit_20200831

--- C/EvenNumbers.c
+++ C/EvenNumbers.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
-main()
+
+int main()
 {
 	int counter;
-	printf("Even numbers between 1 to 100");
+	printf("Even numbers between 1 to 100 are:\n");
 	
-	for(counter=1;counter>=100;counter++)
+	for(counter=1;counter<=100;counter++)
 	{
+//	counter=2;	
 		if(counter%2==0)
+//	while(counter<=100)
 		{
 			printf("%d,counter");
 		}
-		
-	}
+       // counter= counter+2;
+ 	}
 	return 0;
 }
--- CPP/PrimeNumber.cpp
+++ CPP/PrimeNumber.cpp
@@ -10,6 +10,8 @@ int main()
 	
 	for (int i=2; i<=1000; i++)
 	{
+		flag=0;
+		
 		for (int j=2; j<=i/2; j++)
 		{
 			if( i%j==0)
