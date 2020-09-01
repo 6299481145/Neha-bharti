c967fddd8d273a6a67633acc5120fe767360935b
6299481145 <64630034+6299481145@users.noreply.github.com>
2020-09-01 10:34:16 +0530

    Commit_20200901

--- C/EvenNumbersWhile.c
+++ C/EvenNumbersWhile.c
@@ -6,12 +6,13 @@ main()
 	
 	printf("Even numbers between 1 To 100 are:\n");
 	
+	//Initialize counter with first even number 2, and increament it by 2 in every ireration
 	counter=2;
 	
 	while(counter<=100)
 	{
 		printf("%d,",counter);
-		
+		//	Add 2 to current even number to get next even number
 		counter=counter+2;
 	}
 
