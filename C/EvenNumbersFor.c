874c2a04f50cc03487e23787beb2d9a33fb231b8
6299481145 <64630034+6299481145@users.noreply.github.com>
2020-09-04 22:22:09 +0530

    Commit_20200904

--- C/EvenNumbersFor.c
+++ C/EvenNumbersFor.c
@@ -5,15 +5,16 @@ int main()
 	int counter;
 	printf("Even numbers between 1 to 100 are:\n");
 	
-	for(counter=1;counter<=100;counter++)
+	for(counter=1;counter<=100-1;counter++)
 	{
 	
 		if(counter%2==0)	
 	
 		{
-			printf("%d,",counter);
+			printf("%d,",counter );
 		}
  
  	}
+ 	printf("%d.",counter);
 	return 0;
 }
