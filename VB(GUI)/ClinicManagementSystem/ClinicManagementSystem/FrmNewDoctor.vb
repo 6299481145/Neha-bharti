790c93bbba820a8801bce91bc19493e022b1db91
6299481145 <64630034+6299481145@users.noreply.github.com>
2020-09-20 12:52:30 +0530

    Commit_20200920

--- VB(GUI)/ClinicManagementSystem/ClinicManagementSystem/FrmNewDoctor.vb
+++ VB(GUI)/ClinicManagementSystem/ClinicManagementSystem/FrmNewDoctor.vb
@@ -1,4 +1,6 @@
-﻿Public Class FrmNewDoctor
+﻿Imports System.Data.SqlClient
+Imports System.IO
+Public Class FrmNewDoctor
     Private Sub FrmNewDoctor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.MdiParent = FrmMDI
 
@@ -17,5 +19,16 @@
     Private Sub LlbBrower_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LlbBrower.LinkClicked
         Dim ofd As New OpenFileDialog()
         ofd.ShowDialog()
+    End Sub
+
+    Private Sub BtnSubmit_Click(sender As Object, e As EventArgs) Handles BtnSubmit.Click
+        Dim con As New sqlconnection("server=(localdb)\mssqllocaldb;Database=Clinic")
+        Dim cmd As New SqlCommand()
+        con.Open()
+        cmd.Connection = con
+        cmd.CommandText = "INSERT INTO Doctor VALUES('D001','Neha Bharti','F',2001-10-12')"
+        cmd.ExecuteNonQuery()
+        con.Close()
+        MessageBox.Show("Data inserted successfully!")
     End Sub
 End Class
