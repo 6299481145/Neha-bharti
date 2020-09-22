af0521f8f8d986bb4d7339553fadaba68690cfea
6299481145 <64630034+6299481145@users.noreply.github.com>
2020-09-22 10:43:38 +0530

    Commit_20200922

---
+++ BookExample/CPP/Chapter-11(Files)/ReadingWritingObjects.cpp
@@ -0,0 +1,59 @@
+//Program 11.6 Reading and Writing Class Objects
+
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+
+using namespace std;
+
+class INVENTORY
+{
+	int code;
+	char name[10];
+	float cost;
+ public:
+    void readdata();
+    void writedata();
+};
+
+void INVENTORY:: readdata()     // read from keyboard
+{
+	cout<<"Enter item code:";
+	cin>>code;
+	cout<<"Enter item name:";
+	cin>>name;
+	cout<<"Enter item cost:";
+	cin>>cost;
+}
+
+void INVENTORY ::writedata()
+{
+	cout<<setw(10)<<code;
+	cout<<setw(10)<<name;
+	cout<<std::fixed<<setprecision(2)<<setw(10)<<cost;
+	cout<<endl;
+}
+
+int main()
+{
+	INVENTORY item[3];
+	
+	fstream file;
+	
+	file.open("STOCK.DAT", ios::in | ios::out);
+	
+	cout<<"ENTER DETAILS FOR THREE ITEMS"<<endl;
+	for(int i=0;i<3;i++)
+	{
+		item[i].readdata();
+		file.write((char*) &item[i], sizeof(item[i]));
+	}
+	cout<<endl<<"OUTPUT"<<endl<<endl;
+	for(int i=0;i<3;i++)
+	{
+		file.read((char*) &item[i], sizeof(item[i]));
+		item[i].writedata();
+	}
+	file.close();
+	return 0;
+}
