6090f42fd25ca179c19145cf221437503173ffb7
6299481145 <64630034+6299481145@users.noreply.github.com>
2020-12-26 10:54:01 +0530

    Commit_20201226

--- VB(GUI)/School Management System/School Management System/FrmMain.vb
+++ VB(GUI)/School Management System/School Management System/FrmMain.vb
@@ -1,6 +1,6 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class FrmMain
-    Dim con As New MySqlConnection("Server = localhost; Database = School; Uid= Neha Bharti; Pwd=Neha@1210")
+    Dim con As New MySqlConnection("Server = localhost; Database = School; Uid= Neha ; Pwd=Neha")
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         con.Open()
         MessageBox.Show("connection Successful")
