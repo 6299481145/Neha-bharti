125a172f82c43a190785c748fbe6a3065c45c827
6299481145 <64630034+6299481145@users.noreply.github.com>
2022-11-08 12:20:57 +0530

    Program to print each word in capital letter.

--- Java/sources/com/nehu/capitalizeword/CapitalizeWord.java
+++ Java/sources/com/nehu/capitalizeword/CapitalizeWord.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class CapitalizeWord {
 
   public static void main(String[] args) {
-    String sentence = "neha bharti is a geneious girl.";
-    String upper_case_sentence = "";
-    Scanner scanner = new Scanner(sentence);
+    // String sentence = "neha bharti is a geneious girl.";
+    String sentence = "";
+    Scanner scanner = new Scanner("neha bharti is a geneious girl.");
     while (scanner.hasNext()) {
       String word = scanner.next();
-      upper_case_sentence +=
+      sentence +=
         Character.toUpperCase(word.charAt(0)) + word.substring(1) + " ";
     }
-    System.out.println(upper_case_sentence);
+    System.out.println(sentence);
   }
 }
